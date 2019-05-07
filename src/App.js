c169052c1bd5478fffdf6b274049708df8b9f130
KingNaranja <kledaa3@gmail.com>
2019-05-07 17:50:00 -0400

    App component is now stateful
    - Refactor and remove unecessary css
    - App state contains user object
    - created gettter and setter methods for user state, as well as a method
      that will clear the user from the state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom'
 // icons 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faGamepad, faDesktop, faLaptop, faLaptopCode, faEnvelope, faKey, faUserEdit } from '@fortawesome/free-solid-svg-icons'
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-function App() {
-   // build a fontaweosme icon library
-  library.add(fab, faGamepad, faDesktop, faLaptop, faLaptopCode, faEnvelope, faKey, faUserEdit )
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      user: null
+    }
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+  setUser = user => this.setState({ user })
+
+  clearUser = () => this.setState({ user: null })
+
+  getUser = () => this.state.user
+
+  render(){
+    const { user } = this.state
+
+    // build a fontawesome icon library
+    library.add(
+      fab, faGamepad,
+      faDesktop, 
+      faLaptop, 
+      faLaptopCode, 
+      faEnvelope, 
+      faKey, 
+      faUserEdit 
+    )
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
