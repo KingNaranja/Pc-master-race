eab23f1e409c9c4044b0a4317eb415cb18b5ed96
KingNaranja <kledaa3@gmail.com>
2019-05-14 15:33:18 -0400

    Move styled header inside functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,34 +5,43 @@ import NavBarContainer from './NavBarContainer';
 // import  NavBarContainer  from './NavBarContainer'
 
 
-const MainHeader = styled.header`
-  position: relative;
-  height: 10vh;
-  width: 100%;
-  background-color: #23242C;
-  align-items: center;
-  display: flex;
-  padding: 0 20px;
-  /* animation-name: descend;
-  animation-duration: 2s; */
-  animation: descend 1s ease-in-out ;
-  .logo{
-    width: 50%;
-    height: auto;
-  }
-  @keyframes descend {
-    from{transform: translateY(-10vh) }
-    to{transform: translateY(0vh)}
-  }
-  
-`
 
-const Header = props  => (
-  <MainHeader>
-    <img className='logo' src={ logo } alt="PCMASTERRACE" />
+
+const Header = props  => {
+
+  const MainHeader = styled.header`
+    position: relative;
+    height: 10vh;
+    width: 100%;
+    background-color: #23242C;
+    align-items: center;
+    display: flex;
+    padding: 0 20px;
+    /* animation-name: descend;
+    animation-duration: 2s; */
+    animation: descend 1s ease-in-out ;
+    .logo{
+      width: 50%;
+      height: auto;
+    }
+    @keyframes descend {
+      from{transform: translateY(-10vh) }
+      to{transform: translateY(0vh)}
+    }
+    .scroll {
+      position: fixed;
+      top: 0;
+      left: 0;
+    }
     
-    <NavBarContainer/>
-  </MainHeader>
-)
+  `
+
+  return (
+    <MainHeader>
+      <img className='logo' src={ logo } alt="PCMASTERRACE" />
+      
+      <NavBarContainer/>
+    </MainHeader>
+)}
 
 export default Header 
